188df641f8c29c7c9fca7a79af7ffcf45d924165
christopherwilliamflores <christopher.flores.ri@gmail.com>
2022-10-12 09:50:36 -0500

    Changes

---
+++ introduction/src/App.js
@@ -0,0 +1,37 @@
+import react from "react";
+import {TodoCounter} from './TodoCounter.js';
+import {TodoSearch} from './TodoSearch.js';
+import {TodoList} from './TodoList.js';
+import {TodoItem} from './TodoItem.js';
+import { CreateTodoButtom }from "./CreateTodoButton.js";
+
+
+//import logo from './logo.svg';
+//import './App.css';
+
+const todos = [
+  {text:'Cortar cebolla', completed:false},
+  {text:'Tormar el curso de intro a react', completed:false},
+  {text:'Llorar con la llorona', completed:false}
+];
+
+function App() {
+return (
+   <react.Fragment>
+      <TodoCounter />
+
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map(todo => (
+            <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+
+      <CreateTodoButtom />
+      
+   </react.Fragment>
+  );
+}
+
+export default App;
